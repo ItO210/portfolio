5958282c592a6ba4867a7b1e8536bd28f07994a2
Carlos Ito <ito@Carloss-MacBook-Pro.local>
2023-01-24 12:57:10 -0600

    bolierplate components for projects and intro

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Terminal Website",
+        imgUrl: "",
+        stack: ["HTML", "CSS", "JavaScript"],
+        link: "",
+    },
+    {
+        title: "Developer Portfolio",
+        imgUrl: "",
+        stack: ["React", "Tailwind", "Vite"],
+        link: "",
+    },
+    {
+        title: "AI Shit",
+        imgUrl: "",
+        stack: ["Python", "C++", "IDK"],
+        link: "",
+    },
+]
