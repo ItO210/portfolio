db410df403825fcb22ba1bbfe8a3a6a1dda3ddfc
Carlos Ito <ito@Carloss-MacBook-Pro.local>
2023-01-27 14:16:23 -0600

    testing language translation with i18next

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,7 +3,7 @@ export default [
         title: "Terminal Website",
         imgUrl: "/assets/terminal-website.jpg",
         stack: ["HTML", "CSS", "JavaScript"],
-        link: "https://google.com",
+        link: "",
     },
     {
         title: "Developer Portfolio",
