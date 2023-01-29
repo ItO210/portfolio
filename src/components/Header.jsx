181ab9306b3e49ff8e378bd27db1b2f638edd4e4
Carlos Ito <ito@Carloss-MacBook-Pro.local>
2023-01-29 12:51:33 -0600

    tried moving darkmode to a separate file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import handleThemeSwitch, {sun, moon} from "../darkmode"
 
 export default function Header() {
+    
     const [t, i18n] = useTranslation("global");
+
     return(
         <div>
             <h1>{t("header.hello-world")}</h1>
             <button onClick={() => i18n.changeLanguage("es")}>ES</button>
             <button onClick={() => i18n.changeLanguage("en")}>EN</button>
+            <button 
+            type="button"
+            onClick={handleThemeSwitch}
+            className="fixed p-2 z-10 right-20 top-4 bg-violet-300 dark:bg-orange-300 text-lg p-1 rounded-md">
+                {theme === "dark" ? sun : moon}
+            </button>
         </div>
+        
     );
 }
